06393d7db3aed607015b1dc15c3757396c4b0e7e
printf-gollo <al551305@edu.uaa.mx>
2025-06-24 19:13:32 -0600

    ProyectoFinal2do_Laberinto.cpp
    version final notas:
    correccion de validaciones.
    comentarios añadidos.
    correccion del problema en matrizmarcada.
    100% completado

--- ProyectoFinal2do_Laberinto.cpp
+++ ProyectoFinal2do_Laberinto.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
-#include <time.h>
-#include <string.h>
+#include <time.h> // Para numeros random
+#include <string.h> // Para validaciones
 
 using namespace std;
 
@@ -17,47 +17,48 @@ union Datos{
 
 // Variables globales 
 int reng, colu;
-Posicion ultimaPos = {-1, -1}; // inicializar union
+Posicion ultimaPos = {-1, -1}; // inicializar struct
 int chacales = 0; // contador de chacales
 
 // Prototipos
 int** llenar();
 void mostrar(int **mat);
-int suma(int **mat);
-int suma2(int **mat);
+int sumaDiagonal(int **mat);
+int sumaColumna(int **mat);
 void liberar(int **mat);
 bool dimension();
-void grabar(); // Grabar en el archivo
-void imprimir(int **mat, int sumTot); // Imprimir en el archivo
+void grabarArchivo(); // Grabar en el archivo
+void imprimirArchivo(int **mat, int sumTot); // Imprimir en el archivo
+bool** marcarCelda(int **mat);
 
 // Main
 int main(){
     int sumTot=0, sumTot2 = 0, sumFin=0;
-    bool a; // Variable que guarda si es que se admitio la captura de datos
+    bool admitido; // Variable que guarda si es que se admitio la captura de datos
 
     srand(time(NULL)); // semilla del rand
 
-    a=dimension(); // Pedir el tamaño al usuario
+    admitido = dimension(); // Pedir el tamaño al usuario
     
     int **mat = llenar(); // Llenar la matriz
 
-    if(a){
+    if(admitido){
         mostrar(mat); // Mostrar la matriz
 
-        sumTot = suma(mat); // Sumar y buscar chacales (proceso general)
-        sumTot2 = suma2(mat);
+        sumTot = sumaDiagonal(mat); // Sumar y buscar chacales (proceso general)
+        sumTot2 = sumaColumna(mat); // Sumar y buscar chacales (proceso general)
 
         sumFin = sumTot + sumTot2;
         cout << "Suma total: " << sumFin<< endl;
 
         // Mostrar si es que se llego al limite de chacales
         if(ultimaPos.fila != -1 && ultimaPos.columna != -1){
-            cout << "Ultima posicion antes de detenerse:\n";
+            cout << "Ultima posicion antes de detenerse:" << endl;
             cout << "Renglon: " << ultimaPos.fila << "\tColumna: " << ultimaPos.columna << endl;
         }
 
         // Imprimir en el archivo
-        imprimir(mat, sumFin);
+        imprimirArchivo(mat, sumFin);
     }
     
 
@@ -85,18 +86,33 @@ int **llenar(){
 
 // Mostrar contenido de la matriz
 void mostrar(int **mat){
-    for(int i=0; i<reng; i++){
-        for(int j=0; j<colu; j++){
-            cout << (*(*(mat + i) + j)) << "\t";
+    bool **marcada = marcarCelda(mat);
+
+    for(int i = 0; i < reng; i++){
+
+        if(reng == 15) cout << "\t\t";
+        else cout << "\t\t\t\t";
+        for(int j = 0; j < colu; j++){
+            if(marcada[i][j]){
+                // Color de fondo azul y texto blanco
+                cout << "\033[1;37;44m" << mat[i][j] << "\033[0m\t";
+            }else{
+                cout << mat[i][j] << "\t";
+            }
         }
         cout << endl;
     }
     cout << endl;
+
+    // Liberar memoria usada por la matriz marcada
+    for(int i = 0; i < reng; i++) delete[] marcada[i];
+    delete[] marcada;
+    marcada = nullptr;
 }
 
 // Funcion para definir el tamaño de la matriz usada
 bool dimension(){
-    grabar();
+    grabarArchivo();
     FILE *archivo; // Declarar el archivo
     
 
@@ -133,16 +149,15 @@ bool dimension(){
     return true;
 }
 
-int suma(int **mat){
+int sumaDiagonal(int **mat){
     Datos sumTot; // Variable de tipo Datos para almacenar la suma total
     sumTot.sumaTotal = 0; // Inicializar en 0 el acomulador
     chacales = 0; // Reiniciar el contador de ceros
 
     // Sumar la diagonal principal y contar chacales
     for(int i=0; i<reng; ++i){
-        if((*(*(mat + i) + i)) == 0){
-            chacales++; // Aumentar el contador de chacales
-        }
+        if((*(*(mat + i) + i)) == 0) chacales++; // Aumentar el contador de chacales
+
         if(chacales == 3){
             ultimaPos.fila = i; // REN
             ultimaPos.columna = i; // COL
@@ -154,15 +169,14 @@ int suma(int **mat){
     return sumTot.sumaTotal;
 }
 
-int suma2(int **mat){
+int sumaColumna(int **mat){
     Datos sumTot2;
     sumTot2.sumaTotal = 0; // Definir en 0 el acomulador
 
     if(chacales < 3){
         for(int i = colu-2; i>=0; i--){  // Recorre de abajo hacia arriba
-            if((*(*(mat + i) + colu - 1)) == 0){
-                chacales++; // Aumentar el contador de chacales
-            }else{
+            if((*(*(mat + i) + colu - 1)) == 0) chacales++; // Aumentar el contador de chacales
+            else{
                 if(chacales == 3){
                     ultimaPos.fila = i+1; // REN
                     ultimaPos.columna = colu - 1; // COL
@@ -184,10 +198,11 @@ void liberar(int **mat){
         delete[] (*(mat +i));
     }
     delete[] mat;
+    mat = nullptr;
 }
 
 // Funcion para grabar los renglones y columnas en el archivo
-void grabar(){
+void grabarArchivo(){
     FILE *archivo;
     char entrada[3];
     int ren, col;
@@ -199,12 +214,12 @@ void grabar(){
     }
 
     // Pedir renglones
-    cout << "Numero de renglones: ";
+    cout << "Numero de renglones (min 5, max 15): ";
     cin >> entrada;
 
     // Verificar que se ingrese solamente enteros y positivos
-    for(int i=0; entrada[i]!='\0'; i++){
-        if(!isdigit(entrada[i])){
+    for(int i=0; (*(entrada + i))!='\0'; i++){
+        if(!isdigit((*(entrada + i)))){
             cout << "Entrada invalida, solo se permiten enteros positivos" << endl;
             fclose(archivo);
             return;
@@ -214,12 +229,12 @@ void grabar(){
     fprintf(archivo, "%d\n", ren); // grabar en el archivo
 
     // Pedir columnas
-    cout << "Numero de columnas: ";
+    cout << "Numero de columnas (min 5, max 15): ";
     cin >> entrada;
 
     // Verificar que se ingrese solamente enteros y positivos
-    for(int i=0; entrada[i]!='\0'; i++){
-        if(!isdigit(entrada[i])){
+    for(int i=0; (*(entrada + i))!='\0'; i++){
+        if(!isdigit((*(entrada + i)))){
             cout << "Entrada invalida, solo se permiten enteros positivos" << endl;
             fclose(archivo);
             return;
@@ -232,7 +247,7 @@ void grabar(){
 }
 
 // imprimir la salida en el archivo
-void imprimir(int **mat, int sumTot){
+void imprimirArchivo(int **mat, int sumTot){
     FILE *archivo;  //variable para manejar el archivo
 
     // Abrir archivo
@@ -259,4 +274,37 @@ void imprimir(int **mat, int sumTot){
 
 	// Cerrar archivo 
 	fclose(archivo);
+}
+
+
+// Funcion auxiliar para saber que posiciones marcar
+bool** marcarCelda(int **mat){
+    bool** marcada = new bool*[reng];
+    for(int i = 0; i < reng; ++i) marcada[i] = new bool[colu](); // Inicializa en false
+
+    int chacalesTemp = 0;
+
+    // Recorrido por la diagonal principal
+    for(int i = 0; i < reng; ++i){
+        if(mat[i][i] == 0){
+            chacalesTemp++;
+            if(chacalesTemp == 3) break; // No marcar el tercer chacal
+        }else{
+            marcada[i][i] = true;
+        }
+    }
+
+    // Recorrido por la ultima columna (excepto esquina inferior derecha)
+    if(chacalesTemp < 3){
+        for(int i = colu - 2; i >= 0; --i){
+            if(mat[i][colu - 1] == 0){
+                chacalesTemp++;
+                if(chacalesTemp == 3) break; // No marcar el tercer chacal
+            }else{
+                marcada[i][colu - 1] = true;
+            }
+        }
+    }
+
+    return marcada;
 }
