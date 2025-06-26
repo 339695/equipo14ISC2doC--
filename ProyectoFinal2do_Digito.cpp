cd9909f1a5f933ac258fad01e6cebef202616356
287994 <al287994@edu.uaa.mx>
2025-06-26 10:50:57 -0600

    Update ProyectoFinal2do_Digito.cpp
    comentado del codigo

--- ProyectoFinal2do_Digito.cpp
+++ ProyectoFinal2do_Digito.cpp
@@ -2,60 +2,72 @@
 por: Equipo 14 ISC 2c
 Ejercicio #4: Digito*/
 #include <iostream>
+#include <windows.h>
 using namespace std;
-void ISBN(long long codigo,int A[]);
-bool ISBNValido(int A[]);
+void ISBN(long long codigo,int* isbn);
+bool ISBNValido(int* isbn);
+void setColor(int);
+
 int main(){
     long long codigo;
-    int A[13];
+    int* isbn=new int[13];
     FILE *archivo;
-    archivo=fopen("ISBN.txt","r");
-    if(archivo==NULL){
-        cout<<"Error al abrir el archivo."<<endl;
+    archivo=fopen("ISBN.txt","r");  //abre el archivo en modo lectura
+    if(archivo==NULL){      //valida que se pueda abrir el archivo
+        setColor(4);
+        cout<<"Error al abrir el archivo."<<endl;   
         return 1;
     }
-    while(fscanf(archivo,"%lld",&codigo)!=EOF){
-        ISBN(codigo, A);
+    while(fscanf(archivo,"%lld",&codigo)!=EOF){   //guarda en variables lo que reciba de cada linea del archivo   
+        ISBN(codigo, isbn);        //llama a la funcion isbns con los argumentos que acaba de recibir
     }
-        fclose(archivo);
-        return 0;
+    fclose(archivo);    //cierra el archivo 
+    delete(isbn);       //cierra la memoria dinamica
+    return 0;           
 }
-void ISBN(long long codigo, int A[]){
-    //cada digito se guarda en el arreglo
+
+void setColor(int color){               
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color);    //funcion propia de windows.h para cambiar el color de la letra
+}   
+
+
+void ISBN(long long codigo, int* isbn){
     FILE *archivo;
     archivo=fopen("ISBN.txt","a");
     for(int i=0;i<13;i++,codigo/=10){
-        A[(13-1)-i]=codigo%10;
+        isbn[(13-1)-i]=codigo%10;       //cada digito se guarda en el arreglo
     }
-    // mostrar el ISBN desglosado
+    setColor(7);
     cout<<"ISBN descompuesto: ";
     for(int i=0;i<13;i++){
-        cout<<A[i];
+        cout<<isbn[i];          // mostrar el ISBN desglosado
     }
     cout<<endl;
-    if(ISBNValido(A)){
-        cout<<"El ISBN es valido"<<endl;
-        //fprintf(archivo,"ISBN valido: \n");
+    if(ISBNValido(isbn)){   //si es ISBN es valido: 
+        setColor(10);
+        cout<<"El ISBN es valido"<<endl;       
     } 
-    else{
-        cout<<"El ISBN no es valido"<<endl;
-        //fprintf(archivo,"ISBN no valido: \n");
+    else{                   //si no es valido
+        setColor(10);
+        cout<<"El ISBN no es valido"<<endl; 
     }
-    fclose(archivo);
+    fclose(archivo);    //cierra el archivo
+    setColor(7);
     cout<<"-----------------------------"<<endl;
 }
-bool ISBNValido(int A[]){
+
+bool ISBNValido(int* isbn){
     int suma=0;
     for(int i=0;i<12;i++){
-        if((i+1)%2!=0){
-            suma+=A[i];
-        } 
+        if((i+1)%2!=0){     //recorre posicion por posicionbuscando pares
+            suma+=isbn[i];  //si es una posicion par en la variable suma 
+        }                   //le suma el valor de la posicion actual del ISBN
         else{
-            suma+=A[i]*3;
-        }
+            suma+=isbn[i]*3;//si es una posicion inpar a la variable suma 
+        }                   //le suma 3 veces el valor de la posicion actual del ISBN
     }
-    int residuo=suma%10;
+    int residuo=suma%10;        
     int resultado=10-residuo;
     int verificador=resultado%10;
-    return verificador==A[12];
+    return verificador==isbn[12];
 }
